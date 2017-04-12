6b77d80423471dc7084d241cd64ae5093ad66639
John Tellep <tellepjc@mail.uc.edu>
2017-04-12 19:57:59 -0400

    Configuring Dataset and pushing web.config

--- App_Code/GetProduct.cs
+++ App_Code/GetProduct.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using dsGetProductTableAdapters;
+using System.Data;
 
 /// <summary>
 /// Summary description for GetProduct
@@ -17,6 +20,9 @@ public class GetProduct
 
     public int RandomProductAvailableAtStore(int storeID)
     {
+        dsGetProductTableAdapters.getRandomProductSoldAtGivenStore productAdapter = new dsGetProductTableAdapters.getRandomProductSoldAtGivenStore();
+        
+
         return 0;
     }
 }
