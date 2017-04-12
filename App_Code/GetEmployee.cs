a870dc0a1112ecf8a1d1fba256787bca3650851c
JeffDowns <Jeffdowns92@gmail.com>
2017-04-12 19:33:29 -0400

    Added Web.config and added method RandomAvailableEmployees() Method

--- App_Code/GetEmployee.cs
+++ App_Code/GetEmployee.cs
@@ -7,34 +7,43 @@ using System.Data.SqlClient;
 /// <summary>
 /// Summary description for GetEmployee
 /// </summary>
-public class GetEmployee
-{
-    public GetEmployee()
-    {
+public class GetEmployee {
+    public GetEmployee() {
         //
         // TODO: Add constructor logic here
         //
     }
 
-    public int RandomAvailableEmployee()
-    {
-        return 0;
+    public int RandomAvailableEmployee() {
+        int emplID = 0;
+
+        OpenConnection("GroceryStoreSimulator");
+
+        try { reader.Close(); } catch { }
+        comm = new SqlCommand("SELECT TOP 1 emp.EmplID FROM tEmpl emp JOIN tEmplHistory eh ON emp.EmplID = eh.EmplID JOIN tEmplStatus es ON eh.EmplStatusID = es.EmplStatusID WHERE es.CanWork = 1 ORDER BY NEWID()", conn);
+        reader = comm.ExecuteReader();
+
+        if (reader.HasRows) {
+            while (reader.Read()) {
+                emplID = (int)reader[0];
+            }
+        }
+
+        return emplID;
     }
 
     private static SqlConnection conn;
     private static SqlDataReader reader;
     private static SqlCommand comm;
 
-    public void OpenConnection(string connStrName)
-    {
+    public void OpenConnection(string connStrName) {
         System.Configuration.ConnectionStringSettings strConn;
         strConn = ReadConnectionString(connStrName);
         conn = new SqlConnection(strConn.ConnectionString);
         conn.Open();
     }
 
-    private System.Configuration.ConnectionStringSettings ReadConnectionString(string connStrName)
-    {
+    private System.Configuration.ConnectionStringSettings ReadConnectionString(string connStrName) {
         string strPath;
         strPath = HttpContext.Current.Request.ApplicationPath + "/Web.config";
         System.Configuration.Configuration rootWebConfig =
@@ -42,17 +51,13 @@ public class GetEmployee
 
         System.Configuration.ConnectionStringSettings connString = null;
 
-        if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
-        {
+        if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0) {
             connString = rootWebConfig.ConnectionStrings.ConnectionStrings[connStrName];
 
-            if (connString != null)
-            {
+            if (connString != null) {
                 Console.WriteLine("connection string = \"{0}\"",
                     connString.ConnectionString);
-            }
-            else
-            {
+            } else {
                 Console.WriteLine("No connection string");
             }
         }
