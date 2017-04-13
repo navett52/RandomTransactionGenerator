24baf9890a80496abf0a4f718cc80f5621654110
navett52 <evantellep@yahoo.com>
2017-04-12 21:00:14 -0400

    -Created GetMiscValues.cs -Created GetLoyaltyID method -Created GetPricePerSellableUnitAsMarked method

---
+++ App_Code/GetMiscValues.cs
@@ -0,0 +1,35 @@
+﻿using dsLoyaltyTableAdapters;
+using dsPricePerSellableUnitAsMarkedTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GetMiscValues
+/// </summary>
+public class GetMiscValues
+{
+    public GetMiscValues()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public int GetLoyaltyID(int storeID)
+    {
+        tLoyaltyTableAdapter loyaltyTableAdapter = new tLoyaltyTableAdapter();
+        dsLoyalty.tLoyaltyDataTable loyaltyDataTable = loyaltyTableAdapter.GetData(storeID);
+
+        return Convert.ToInt32(loyaltyDataTable.Rows[0][0]);
+    }
+
+    public int GetPricePerSellableUnitAsMarked(int storeID, int productID)
+    {
+        tProductPriceHistTableAdapter productPriceHistTableAdapter = new tProductPriceHistTableAdapter();
+        dsPricePerSellableUnitAsMarked.tProductPriceHistDataTable pricePerSellableUnitAsMarkedDataTable = productPriceHistTableAdapter.GetData(storeID, productID);
+
+        return Convert.ToInt32(pricePerSellableUnitAsMarkedDataTable.Rows[0][0]);
+    }
+}
